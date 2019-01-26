544372e67a2b33b8b71557c3cb8e8e87a7f3444f
Greg Burman <burman.greg@gmail.com>
2019-01-27 00:31:31 +0100

    reimplement unet

--- train/mknet.py
+++ train/mknet.py
@@ -1,69 +1,76 @@
 import mala
 import tensorflow as tf
 import json
+import prob_unet
 
 def create_network(input_shape, name):
 
-    tf.reset_default_graph()
+	tf.reset_default_graph()
 
-    raw = tf.placeholder(tf.float32, shape=input_shape)
-    raw_batched = tf.reshape(raw, (1, 1) + input_shape)
-    print type(raw_batched)
+	raw = tf.placeholder(tf.float32, shape=input_shape)
+	raw_batched = tf.reshape(raw, (1, 1) + input_shape)
 
-    unet, _, _ = mala.networks.unet(
-        fmaps_in=raw_batched,
-        num_fmaps=12,
-        fmap_inc_factors=3,
-        downsample_factors=[[3,3,3],[2,2,2],[2,2,2]]) # 2,2,2 etc or 3,3,3 (downsample the same amount in all dimensions  because toy data isotropic)
+	# unet, _, _ = mala.networks.unet(
+	# 	fmaps_in=raw_batched,
+	# 	num_fmaps=12,
+	# 	fmap_inc_factors=3,
+	# 	downsample_factors=[[3,3,3],[2,2,2],[2,2,2]]) # 2,2,2 etc or 3,3,3 (downsample the same amount in all dimensions  because toy data isotropic)
 
-    affs_batched, _ = mala.networks.conv_pass(
-        fmaps_in=unet,
-        kernel_sizes=[1],
-        num_fmaps=3,
-        activation='sigmoid',
-        name='affs')
+	unet = prob_unet.unet(
+		fmaps_in=raw_batched,
+		num_layers=3,
+		base_num_fmaps=12,
+		fmap_inc_factor=3,
+		downsample_factors=[[3,3,3], [2,2,2], [2,2,2]])
 
-    output_shape_batched = affs_batched.get_shape().as_list()
-    output_shape = output_shape_batched[1:] # strip the batch dimension
+	affs_batched, _ = mala.networks.conv_pass(
+		fmaps_in=unet,
+		kernel_sizes=[1],
+		num_fmaps=3,
+		activation='sigmoid',
+		name='affs')
 
-    affs = tf.reshape(affs_batched, output_shape)
+	output_shape_batched = affs_batched.get_shape().as_list()
+	output_shape = output_shape_batched[1:] # strip the batch dimension
 
-    gt_affs = tf.placeholder(tf.float32, shape=output_shape)
-    affs_loss_weights = tf.placeholder(tf.float32, shape=output_shape)
-    
-    loss = tf.losses.mean_squared_error(
-        gt_affs,
-        affs,
-        affs_loss_weights)
+	affs = tf.reshape(affs_batched, output_shape)
 
-    summary = tf.summary.scalar('setup01_eucl_loss', loss)
+	gt_affs = tf.placeholder(tf.float32, shape=output_shape)
+	affs_loss_weights = tf.placeholder(tf.float32, shape=output_shape)
+	
+	loss = tf.losses.mean_squared_error(
+		gt_affs,
+		affs,
+		affs_loss_weights)
 
-    opt = tf.train.AdamOptimizer(
-        learning_rate=0.5e-4,
-        beta1=0.95,
-        beta2=0.999,
-        epsilon=1e-8)
-    optimizer = opt.minimize(loss)
+	summary = tf.summary.scalar('setup01_eucl_loss', loss)
 
-    output_shape = output_shape[1:]
-    print("input shape : %s"%(input_shape,))
-    print("output shape: %s"%(output_shape,))
+	opt = tf.train.AdamOptimizer(
+		learning_rate=0.5e-4,
+		beta1=0.95,
+		beta2=0.999,
+		epsilon=1e-8)
+	optimizer = opt.minimize(loss)
 
-    tf.train.export_meta_graph(filename=name + '.meta')
+	output_shape = output_shape[1:]
+	print("input shape : %s"%(input_shape,))
+	print("output shape: %s"%(output_shape,))
 
-    config = {
-        'raw': raw.name,
-        'affs': affs.name,
-        'gt_affs': gt_affs.name,
-        'affs_loss_weights': affs_loss_weights.name,
-        'loss': loss.name,
-        'optimizer': optimizer.name,
-        'input_shape': input_shape,
-        'output_shape': output_shape,
-        'summary': summary.name
-    }
-    with open(name + '.json', 'w') as f:
-        json.dump(config, f)
+	tf.train.export_meta_graph(filename=name + '.meta')
+
+	config = {
+		'raw': raw.name,
+		'affs': affs.name,
+		'gt_affs': gt_affs.name,
+		'affs_loss_weights': affs_loss_weights.name,
+		'loss': loss.name,
+		'optimizer': optimizer.name,
+		'input_shape': input_shape,
+		'output_shape': output_shape,
+		'summary': summary.name
+	}
+	with open(name + '.json', 'w') as f:
+		json.dump(config, f)
 
 if __name__ == "__main__":
-    create_network((196, 196, 196), 'train_net')
+	create_network((199, 199, 199), 'train_net')
--- train/prob_unet.py
+++ train/prob_unet.py
@@ -1,28 +1,180 @@
+# assume tensorflow 1.3
+
 import tensorflow as tf
 from  tensorflow_probability import distributions as tfd
 
+
 def unet(
-	input,
-	layers,
+	fmaps_in,
+	num_layers,
 	base_num_fmaps,
 	fmap_inc_factor,
-	num_conv_passes,
 	downsample_factors,
-	upsample_factors,
+	padding='valid',
+	num_conv_passes=2,
 	kernel_size_down=[3,3,3],
 	kernel_size_up=[3,3,3],
 	activation='relu',
+	downsample_type="max_pool",
+	upsample_type="conv_transpose",
 	fov=(1, 1, 1),
 	voxel_size=(1, 1, 1)):
 
-	# encode
-	for l in layers:
-		for c in num_conv_passes:
-			# fout = conv pass (fin)
-		# fout = downsample (fin)
+	print "encode"
+	fmaps = fmaps_in
+	num_fmaps = base_num_fmaps
+	across = []
+	print fmaps.shape
+	for layer in range(num_layers):
+		for conv_pass in range(num_conv_passes):
+			fmaps = tf.layers.conv3d(
+				inputs=fmaps,
+				filters=num_fmaps,
+				kernel_size=kernel_size_down[layer],
+				padding=padding,
+				data_format="channels_first",
+				activation=activation,
+				name="enc_conv_pass_%i_%i"%(layer, conv_pass))
+		across.append(fmaps)
 
-	# decode
-	for l in layers:
-		for c in num_conv_passes:
-			# fout = conv pass (fin)
-		# fout = upsample (fin)
+		fmaps = downsample(
+			fmaps_in=fmaps,
+			downsample_type=downsample_type,
+			downsample_factors=downsample_factors[layer],
+			padding=padding,
+			voxel_size=voxel_size,
+			name="enc_downsample_%i"%layer)
+
+		print fmaps.shape
+		num_fmaps *= fmap_inc_factor
+
+		if layer == num_layers-1:
+			for conv_pass in range(num_conv_passes):
+				fmaps = tf.layers.conv3d(
+					inputs=fmaps,
+					filters=num_fmaps,
+					kernel_size=kernel_size_down[layer],
+					padding=padding,
+					data_format="channels_first",
+					activation=activation,
+					name="enc_conv_pass_bottom_%i"%conv_pass)
+
+	print "embedded: ", fmaps.shape
+
+	print "decode"
+	for layer in reversed(range(num_layers)):
+		num_fmaps /= fmap_inc_factor
+		fmaps = upsample(
+			fmaps_in=fmaps,
+			num_fmaps=num_fmaps,
+			upsample_type=upsample_type,
+			upsample_factors=downsample_factors[layer],
+			activation=activation,
+			padding=padding,
+			voxel_size=voxel_size,
+			name="dec_upsample_%i"%layer)
+
+		cropped = crop(across[layer], fmaps.get_shape().as_list())
+		fmaps = tf.concat([cropped, fmaps], 1)
+
+		for conv_pass in range(num_conv_passes):
+			fmaps = tf.layers.conv3d(
+				inputs=fmaps,
+				filters=num_fmaps,
+				kernel_size=kernel_size_up[layer],
+				padding=padding,
+				data_format="channels_first",
+				activation=activation,
+				name="dec_conv_pass_%i_%i"%(layer, conv_pass))
+		print fmaps.shape
+
+	return fmaps
+
+
+def downsample(
+	fmaps_in,
+	downsample_type,
+	downsample_factors,
+	padding,
+	voxel_size,
+	name):
+
+	if downsample_type == "max_pool":
+		fmaps_out = tf.layers.max_pooling3d(
+			inputs=fmaps_in,
+			pool_size=downsample_factors,
+			strides=downsample_factors,
+			padding=padding,
+			data_format='channels_first',
+			name=name)
+	else:
+		raise Exception('unsupported downsample type')
+
+	return fmaps_out
+
+
+def upsample(
+	fmaps_in,
+	num_fmaps,
+	upsample_type,
+	upsample_factors,
+	activation,
+	padding,
+	voxel_size,
+	name):
+
+	if upsample_type == "conv_transpose":
+		fmaps_out = tf.layers.conv3d_transpose(
+			inputs=fmaps_in,
+			filters=num_fmaps,
+			kernel_size=upsample_factors,
+			strides=upsample_factors,
+			padding=padding,
+			data_format='channels_first',
+			activation=activation,
+			name=name)
+	else:
+		raise Exception('unsupported upsample type')
+
+	return fmaps_out
+
+
+def crop(fmaps_in, shape):
+
+	in_shape = fmaps_in.get_shape().as_list()
+
+	offset = [
+		0, # batch
+		0, # channel
+		(in_shape[2] - shape[2])//2, # z
+		(in_shape[3] - shape[3])//2, # y
+		(in_shape[4] - shape[4])//2, # x
+	]
+	size = [
+		in_shape[0],
+		in_shape[1],
+		shape[2],
+		shape[3],
+		shape[4],
+	]
+
+	fmaps_out = tf.slice(fmaps_in, offset, size)
+
+	return fmaps_out
+
+
+if __name__ == "__main__":
+
+	# prior = enc + trans + sampling
+	# posterior = enc + transf + sampling
+	# unet = unet (own enc + dec)
+
+	raw = tf.placeholder(tf.float32, (1,1,196,196,196))
+	gt = tf.placeholder(tf.float32, (1,3, 68, 68, 68))
+
+	unet = unet(
+		fmaps_in=raw,
+		num_layers=3,
+		base_num_fmaps=12,
+		fmap_inc_factor=3,
+		downsample_factors=[[3,3,3], [2,2,2], [2,2,2]])
