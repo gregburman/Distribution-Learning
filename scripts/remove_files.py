10d30ef73d9ccccac26416203735f2eb0d91d7cd
Greg Burman <burman.greg@gmail.com>
2019-03-22 14:18:10 +0100

    remove file write from remove_files script

--- scripts/remove_files.py
+++ scripts/remove_files.py
@@ -4,7 +4,6 @@ sys.path.append('../')
 
 if __name__ == "__main__":
 
-	f = open("files_to_remove", 'w')
 	setup = sys.argv[1]
 	file_type = sys.argv[2]
 	iterations = int(sys.argv[3])
@@ -15,9 +14,7 @@ if __name__ == "__main__":
 		n = i*old_batch_size
 		if n % new_batch_size != 0:
 			if file_type == "checkpoints":
-				name = "train/prob_unet/" + setup + "/weights_checkpoint_%i.*"%n
+				name = "train/prob_unet/" + setup + "/train_net_checkpoint_%i.*"%n
 			elif file_type == "snapshots":
 				name = "snapshots/prob_unet/" + setup + "/batch_%i.hdf"%(n+1)
 			os.system("sudo rm " + name)
-	f.close()
-
